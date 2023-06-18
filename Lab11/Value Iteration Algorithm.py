ffee870d91a875eecf8e467b55aa14330a83284d
s23215 <s23215@pjwstk.edu.pl>
2023-06-18 22:13:04 +0200

    bug fixes :)

--- Lab11/Value Iteration Algorithm.py
+++ Lab11/Value Iteration Algorithm.py
@@ -5,7 +5,6 @@ def value_iteration(env, gamma=0.99, epsilon=1e-6):
     num_states = env.observation_space.n
     num_actions = env.action_space.n
 
-    # Inicjalizacja wartości stanów
     V = np.zeros(num_states)
 
     while True:
@@ -15,14 +14,12 @@ def value_iteration(env, gamma=0.99, epsilon=1e-6):
             v = V[s]
             q_values = []
 
-            # Obliczanie wartości dla każdej akcji w danym stanie
             for a in range(num_actions):
                 q_value = 0
                 for prob, next_state, reward, done in env.P[s][a]:
                     q_value += prob * (reward + gamma * V[next_state])
                 q_values.append(q_value)
 
-            # Aktualizacja wartości stanu na podstawie maksymalnej wartości akcji
             V[s] = max(q_values)
 
             delta = max(delta, abs(v - V[s]))
@@ -30,7 +27,6 @@ def value_iteration(env, gamma=0.99, epsilon=1e-6):
         if delta < epsilon:
             break
 
-    # Znalezienie optymalnej polityki na podstawie estymowanych wartości stanów
     policy = np.zeros(num_states, dtype=int)
     for s in range(num_states):
         q_values = []
@@ -44,24 +40,19 @@ def value_iteration(env, gamma=0.99, epsilon=1e-6):
     return V, policy
 
 
-# Tworzenie środowiska Frozen Lake
-env = gym.make("FrozenLake8x8-v1", render_mode="human")
+env = gym.make("FrozenLake8x8-v1", render_mode="human", is_slippery=False)
 
-# Wywołanie algorytmu Value Iteration
 values, optimal_policy = value_iteration(env)
 
-# Wyświetlenie estymowanych wartości stanów
 print("Estymowane wartości stanów:")
 print(values)
 
-# Wyświetlenie optymalnej polityki
 print("Optymalna polityka:")
 print(optimal_policy)
 
-env.reset()
+state, info = env.reset()
 
 done = False
 
 while not done:
-    for policy in optimal_policy:
-        _, _, done, _, _ = env.step(policy)
+    state, _, done, _, _ = env.step(optimal_policy[state])
